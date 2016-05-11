04855abdaafbb25b079828b6fb27e887a90f9de8
Jamone Kelly <xg.monie089@gmail.com>
2016-05-11 16:19:58 -0400

    Initial Commit

---
+++ Text SelectionTests/Text_SelectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Text_SelectionTests.swift
+//  Text SelectionTests
+//
+//  Created by Jamone Alexander Kelly on 5/11/16.
+//  Copyright © 2016 Jamone. All rights reserved.
+//
+
+import XCTest
+@testable import Text_Selection
+
+class Text_SelectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
