504ea46a72dc1f2fe00e1b080c2b170dcb4bd6c8
Jamone Kelly <xg.monie089@gmail.com>
2016-05-11 16:28:51 -0400

    - Add panGesture to text view - Add dummy text

--- Text Selection/ViewController.swift
+++ Text Selection/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.textView.panGestureRecognizer.minimumNumberOfTouches = 2
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
